bfe0a64df49088e0f4de42964040b3301d9cb45e
med <44341982+med499@users.noreply.github.com>
2022-04-24 18:22:13 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 // Objet geojson des arrondissements de Paris
 var arrondissements = JSON.parse(arrondissementsGeoJson);
 // Objet geojson des zones touristiques de Paris
-var routes = JSON.parse(zonesTouristiquesGeojson);
+var zonesTouristiques = JSON.parse(zonesTouristiquesGeojson);
 
 console.log(arrondissements);
-console.log(routes);
+console.log(zonesTouristiques);
